2fdf7fe7bc8bc6d4da1917e77392bb4a3aff2033
Ubuntu <ubuntu@ip-172-31-23-221.ec2.internal>
2026-03-17 05:15:23 +0000

    Updated port to 8080 and added health endpoint

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ app.use(express.json());
 let todos = [];
 
 app.get('/', (req, res) => {
-  res.json({ message: "Todo API Running Successfully" });
+  res.json({ message: "Todo API v1 Running" });
 });
 
 app.get('/todos', (req, res) => {
   res.json(todos);
+});
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
 app.post('/todos', (req, res) => {
